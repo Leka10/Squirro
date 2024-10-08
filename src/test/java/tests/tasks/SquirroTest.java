cf1aba0ecabbee2fbaf805f31dfd78d0523be832
Aleksa <aleksamujovic@gmail.com>
2024-10-08 06:06:27 +0200

    KP commit

--- src/test/java/tests/tasks/SquirroTest.java
+++ src/test/java/tests/tasks/SquirroTest.java
@@ -9,7 +9,7 @@ public class SquirroTest extends BaseTestClass {
 
 
 
-    @Test
+    @Test(priority = 1)
     public void testCreatingAnAccountWithValidInformation() throws InterruptedException {
 
             HomePage homePage = new HomePage(driver).open();
@@ -42,7 +42,7 @@ public class SquirroTest extends BaseTestClass {
 //        Thread.sleep(2000);
 //    }
 
-    @Test
+    @Test(priority = 2)
     public void testAccountCreationWithInvalidEmailFormats() throws InterruptedException {
 
         HomePage homePage = new HomePage(driver).open();
@@ -56,30 +56,30 @@ public class SquirroTest extends BaseTestClass {
         homePage.enterConfirmPassword("passtest");
         homePage.clickCheckBoxTermsAndCond();
         homePage.clickSubmitButton();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         homePage.enterEmail("test.com");
         homePage.enterPassword("passtest");
         homePage.enterConfirmPassword("passtest");
         homePage.clickCheckBoxTermsAndCond();
         homePage.clickSubmitButton();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         homePage.enterEmail("test@");
         homePage.enterPassword("passtest");
         homePage.enterConfirmPassword("passtest");
         homePage.clickCheckBoxTermsAndCond();
         homePage.clickSubmitButton();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         homePage.enterEmail("@test");
         homePage.enterPassword("passtest");
         homePage.enterConfirmPassword("passtest");
         homePage.clickCheckBoxTermsAndCond();
         homePage.clickSubmitButton();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         Assert.assertTrue(homePage.isInvalidEmailDisplayed());
 
     }
 
-    @Test
+    @Test(priority = 3)
     public void attempToCreateDuplicateAccountsWithTheSameEmail() throws InterruptedException {
 
             HomePage homePage = new HomePage(driver).open();
@@ -93,12 +93,12 @@ public class SquirroTest extends BaseTestClass {
             homePage.enterConfirmPassword("passtest");
             homePage.clickCheckBoxTermsAndCond();
             homePage.clickSubmitButton();
-            Thread.sleep(2000);
+            Thread.sleep(1000);
             Assert.assertTrue(homePage.isExistingEmailDisplayed());
 
     }
 
-    @Test
+    @Test(priority = 4)
     public void testLoginWithValidCredentials() throws InterruptedException {
 
         HomePage homePage = new HomePage(driver).open();
@@ -107,11 +107,11 @@ public class SquirroTest extends BaseTestClass {
         homePage.enterUsername("aleksatest111@gmail.com");
         homePage.enterPassword("passtest");
         homePage.clickSubmitButton();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         Assert.assertTrue(homePage.isAvatarIconDisplayed());
 
     }
-    @Test
+    @Test(priority = 5)
     public void testLoginWithIncorrectPassword() throws InterruptedException {
 
         HomePage homePage = new HomePage(driver).open();
@@ -120,11 +120,11 @@ public class SquirroTest extends BaseTestClass {
         homePage.enterUsername("aleksatest111@gmail.com");
         homePage.enterPassword("testpass");
         homePage.clickSubmitButton();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         Assert.assertTrue(homePage.isInvalidPasswordMessageDisplayed());
 
     }
-    @Test
+    @Test(priority = 6)
     public void verifyForgotPasswordFunctionality() throws InterruptedException {
 
         HomePage homePage = new HomePage(driver).open();
@@ -133,8 +133,52 @@ public class SquirroTest extends BaseTestClass {
         homePage.clickForgotPasswordLink();
         homePage.enterUsername("aleksatest111@gmail.com");
         homePage.clickSubmitButton();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         Assert.assertTrue(homePage.isForgotPasswordMessageDisplayed());
+
+    }
+    //This test fails as it should be because account is never locked and we dont get different message
+    @Test(priority = 7)
+    public void testAccountLockoutAfterMultipleFailedAttempts() throws InterruptedException {
+
+        HomePage homePage = new HomePage(driver).open();
+
+        homePage.clickOnMyInstances();
+        homePage.enterUsername("aleksatest111@gmail.com");
+        homePage.enterPassword("testpass");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass1");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass2");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass3");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass4");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass5");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass6");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass7");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass8");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass9");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass10");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass11");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass12");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass13");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass14");
+        homePage.clickSubmitButton();
+        homePage.enterPassword("testpass15");
+        homePage.clickSubmitButton();
+        Thread.sleep(1000);
+        Assert.assertFalse(homePage.isInvalidPasswordMessageDisplayed());
 
     }
 
